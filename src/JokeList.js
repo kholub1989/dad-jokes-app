f542f7e37dde402a02237abbcb2956fa5ed0d350
Krisztian Holub <kholub1989@gmail.com>
2019-11-06 12:56:43 -0500

    style the Fetch Button

--- src/JokeList.js
+++ src/JokeList.js
@@ -76,7 +76,7 @@ class JokeList extends Component {
             <span>Dad</span> Jokes
           </h1>
           <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-          <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>Fetch Jokes</button>
         </div>  
         <div className="JokeList-jokes">
           {jokes.map(j => (
